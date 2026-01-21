4fab7113b8d7a354e790d138944b10b87afc9d1a
shandohowell <shandohowell94@gmail.com>
2026-01-21 06:47:34 -0500

    Worked on the logic for the Chat Interface component.

--- components/ChatInterface.tsx
+++ components/ChatInterface.tsx
@@ -4,6 +4,7 @@ import { Id, Doc } from "@/convex/_generated/dataModel";
 import { useState, useEffect, useRef } from "react";
 import { Button } from "./ui/button";
 import { api } from "@/convex/_generated/api";
+import { ChatRequestBody } from "@/lib/types";
 
 interface ChatInterfaceProps {
     chatId: Id<"chats">,
@@ -44,4 +45,61 @@ const ChatInterface = ({chatId, initialMessages}: ChatInterfaceProps) => {
     useEffect(() => {
         messagesEndRef.current?.scrollIntoView({behavior: "smooth"})
     }, [messages, setMessages]);
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        // Trim user input
+        const trimmedInput = input.trim();
+        if (!trimmedInput) return;
+
+        // Reset user state for new message
+        setInput("");
+        setStreamedResponse("");
+        setCurrentTool(null);
+        setIsLoading(true);
+
+        // Show user message for better UX
+        const optimisticUserMessage: Doc<"messages"> = {
+            _id: `temp_${Date.now()}`,
+            chatId,
+            role: "user",
+            content: trimmedInput,
+            createdAt: Date.now()
+        } as Doc<"messages">;
+
+        // Track full response for adding to the DB
+        let fullResponse = "";
+
+        // Start streaming
+        try {
+            const requestBody: ChatRequestBody = {
+                messages: messages.map((msg) => ({
+                    role: msg.role,
+                    content: msg.content
+                })),
+                newMessage: trimmedInput,
+                chatId
+            }
+
+            // Initialize SSE connection
+            const response = await fetch("/api/chat/stream", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(requestBody),
+            });
+
+            if (!response.ok) {
+                throw new Error(await response.text());
+            }
+
+            if (!response.body) {
+                throw new Error("No res body available.")
+            }
+        } catch {
+
+        }
+    }
 }
