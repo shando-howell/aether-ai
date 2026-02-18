d17975ed531e9ac958070fa72dfe40d7869dd76a
shandohowell <shandohowell94@gmail.com>
2026-02-18 16:00:14 -0500

    Worked on home page.

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             <p className="text-gray-400 text-2xl">Engineered for Coding</p>
             <div className="flex justify-center">
               <div className="py-2">
-                <CpuIcon className="h-40 w-40 text-green-600" />
+                <CpuIcon className="h-30 w-30 text-green-600" />
               </div>
             </div>
           </div>
@@ -52,7 +52,7 @@ export default function Home() {
             <p className="text-gray-400 text-2xl">Premium Responses</p>
             <div className="flex justify-center">
               <div className="py-2">
-                <MessagesSquare className="h-40 w-40 text-green-600" />
+                <MessagesSquare className="h-30 w-30 text-green-600" />
               </div>
             </div>
           </div>
@@ -62,7 +62,7 @@ export default function Home() {
             <p className="text-gray-400 text-2xl">Powered by Anthropic</p>
             <div className="flex justify-center">
               <div className="py-2">
-                <Brain className="h-40 w-40 text-green-600" />
+                <Brain className="h-30 w-30 text-green-600" />
               </div>
             </div>
           </div>
